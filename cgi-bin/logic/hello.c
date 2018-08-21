b96ca02220189442f310231ee7ef961c25d513ee
lialvin <lialvin@qq.com>
2018-08-21 10:00:33 +0800

    h

--- cgi-bin/logic/hello.c
+++ cgi-bin/logic/hello.c
@@ -1,21 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "inifile.h"
-
-#define DEV_FILE  "/home/ulord/.ulordcore/ulord.conf"
-
-void config_UlordData_Read(  )
-{
-   
-   char  user[20]; 	
-   char  password[20]="abcdefg"; 	
-   
-   read_profile_string_nosection("rpcuser",user,sizeof(user),NULL,DEV_FILE);
-   write_profile_string_nosection("server",password,DEV_FILE);
-   write_profile_string_nosection("rpcuser",password,DEV_FILE);
-} 
+#include<stdio.h>
+#include<stdlib.h>
 
 int main()
 {
@@ -24,8 +10,24 @@ int main()
     char buffer[200];
     char buffer1[100];
 
-    config_UlordData_Read();
-
+    char *home;
+    home = getenv("HOME");
+    printf("the home path is %s\n", home);
+    sprintf(buffer1, "%s/log/boa.log" , home );
+    
+    printf("pwd=%s",buffer1);
+    printf("content-type:text/html;charset=UTF-8\n\n");
+    printf("<TITLE>login: 11111111111 result</TITLE>");
+    printf("<H3>登陆结果</h3>");
+    date=getenv("QUERY_STRING");
+    if(date==NULL)
+        printf("<p>错误：数据没有被输入或数据传输发生错误</p >");
+    else
+    {
+        sscanf(date,"name=%[^&]&pwd=%s",name,pwd);
+        printf("<p>name=%s</p >",name);
+        printf("<p>pwd=%s</p >",pwd);
+        printf("%s",date);
+    }
     return 0;
 }
-
