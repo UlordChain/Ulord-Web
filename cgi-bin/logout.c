94746711182eed52e86b17d22e376a524f35338a
lialvin <lialvin@qq.com>
2018-08-21 11:11:11 +0800

    logout

--- cgi-bin/logout.c
+++ cgi-bin/logout.c
@@ -8,6 +8,8 @@ int cgiMain()
 	char sessionid[40];
 	cgiFormString("para",sessionid,sizeof(sessionid));
 	int int_session=atoi(sessionid);
+    DEBUGINFO("logout cgi  cgimain call  \n");
+    cgiHeaderContentType("application/json");
 	if(TerminateSession(int_session))
 	{
 	    fprintf(cgiOut,"{\"msg\":\"success\",\"status\":1}");
